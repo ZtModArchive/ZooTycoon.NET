bf54c39061e68c0065465fa7e8a9e367223b4276
Apodemus Erectus <bnkho98@hotmail.com>
2022-09-18 23:00:55 +0200

    add scenery

--- ZooTycoon.NET/DataModels/Sections/Characteristics/SceneryCharacteristics.cs
+++ ZooTycoon.NET/DataModels/Sections/Characteristics/SceneryCharacteristics.cs
@@ -1,6 +1,16 @@
-﻿namespace ZooTycoon.NET.DataModels.Sections.Characteristics
+﻿using ZooTycoon.NET.DataModels.Enums;
+
+namespace ZooTycoon.NET.DataModels.Sections.Characteristics
 {
     public class SceneryCharacteristics : Characteristics
     {
+        public int? NameID { get; set; }
+        public int? HelpID { get; set; }
+        public int? PurchaseCost { get; set; }
+        public Habitat Habitat { get; set; }
+        public int? FootprintX { get; set; }
+        public int? FootprintY { get; set; }
+        public int? Height { get; set; }
+        public bool? Walkable { get; set; }
     }
 }
--- ZooTycoon.NET/Entities/Scenery/Scenery.cs
+++ ZooTycoon.NET/Entities/Scenery/Scenery.cs
@@ -5,5 +5,21 @@ namespace ZooTycoon.NET.Entities.Scenery
 {
     public class Scenery : Entity<ScenerySubType, SceneryCharacteristics>
     {
+        public Scenery(string type) : this()
+        {
+            Type = type;
+        }
+
+        public Scenery(string type, ScenerySubType subType) : this()
+        {
+            Type = type;
+            SubType = subType;
+        }
+
+        public Scenery()
+        {
+        }
+
+        public Dictionary<GuestSubTypes, int>? EstheticBonus { get; set; }
     }
 }
